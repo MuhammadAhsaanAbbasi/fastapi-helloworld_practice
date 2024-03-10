0869dde717ca33d4e3612b48de9dcbb6a7660d6f
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2024-03-10 09:36:42 +0500

    todo application

--- fastapi_helloworld/main.py
+++ fastapi_helloworld/main.py
@@ -1,27 +1,70 @@
-from fastapi import FastAPI
-from enum import Enum
+from contextlib import asynccontextmanager
+from typing import Union, Optional, Annotated
+from fastapi_helloworld import settings
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from fastapi import FastAPI, Depends
 
-fastapp = FastAPI(
-    title="Hello World",
+
+class Todo(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    content: str = Field(index=True)
+
+
+# only needed for psycopg 3 - replace postgresql
+# with postgresql+psycopg in settings.DATABASE_URL
+connection_string = str(settings.DATABASE_URL).replace(
+    "postgresql", "postgresql+psycopg"
 )
 
-@fastapp.get('/')
-def get_root():
-    return {"message": "Hello World"}
 
-@fastapp.get("/retrive")
-def get_retrive():
-    return {"message": "Get Retrived!"}
+# recycle connections after 5 minutes
+# to correspond with the compute scale down
+engine = create_engine(
+    connection_string, connect_args={"sslmode": "require"}, pool_recycle=300
+)
 
-@fastapp.get("/products/{product_id}")
-def get_products(product_id: int):
-    return {"message": f"Get Product {product_id}"}
 
-class Product(str, Enum):
-    product1 = "Mobile1",
-    product2 = "Iphone",
-    product3 = "Laptop",
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
 
-@fastapp.get("/devices/{devices_name}")
-def get_devices(device_name: Product):
-    return {"Device_name" : f"You Like a {device_name.value}"}
+
+# The first part of the function, before the yield, will
+# be executed before the application starts.
+# https://fastapi.tiangolo.com/advanced/events/#lifespan-function
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Creating tables..")
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan, title="Hello World API with DB", 
+    version="0.0.1",
+    servers=[
+        {
+            "url": "http://0.0.0.0:8000", # ADD NGROK URL Here Before Creating GPT Action
+            "description": "Development Server"
+        }
+        ])
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+@app.get("/")
+def read_root():
+    return {"Message": "Hello World"}
+
+@app.post("/todos/", response_model=Todo)
+def create_todo(todo: Todo, session: Annotated[Session, Depends(get_session)]):
+        session.add(todo)
+        session.commit()
+        session.refresh(todo)
+        return todo
+
+
+@app.get("/todos/", response_model=list[Todo])
+def read_todos(session: Annotated[Session, Depends(get_session)]):
+        todos = session.exec(select(Todo)).all()
+        return todos
